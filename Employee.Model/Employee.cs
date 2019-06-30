bc9a407ae13c9882b0ba3c31c6c3fbb1d4189af3
Vijay Viswanathan <vijay.viswanathan@amadeus.com>
2019-06-30 10:44:46 +0200

    Add project files.

---
+++ Employee.Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Model
+{
+    public class Employee
+    {
+
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public char Sex { get; set; }
+
+        public string JobTitle { get; set; }
+    }
+}
